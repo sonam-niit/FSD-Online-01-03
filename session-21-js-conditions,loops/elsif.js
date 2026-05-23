1b6a24c43e60a53db9ed5237d1da6e713a0a3f6c
sonam-niit <sonam.gravity@gmail.com>
2026-05-23 16:49:51 +0530

    efelse

---
+++ session-21-js-conditions,loops/elsif.js
@@ -0,0 +1,16 @@
+let per=prompt("Enter Your Percentage")
+//prompt is used to take input from user
+if(per>=90 && per<=100)
+    console.log("Congratulations! You secured Excellent Class")
+else if(per>=70 && per<90)
+    console.log("Congratulations! You secured Distinction Class")
+else if(per>=60 && per<70)
+    console.log("Congratulations! You secured First Class")
+else if(per>=50 && per<60)
+    console.log("Congratulations! You secured Second Class")
+else if(per>=35 && per<50)
+    console.log("Congratulations! You secured Pass Class")
+else if(per<35 && per>=0)
+    console.log("Sorry You are Fail")
+else 
+    console.log("Please Enter Correct Result")
---
+++ session-21-js-conditions,loops/nested.js
@@ -0,0 +1,15 @@
+let a = 15, b = 28, c = 24;
+
+if (a > b) {
+    if (a > c) {
+        console.log(a, " is Largest");
+    } else {
+        console.log(c, " is Largest");
+    }
+} else {
+    if (b > c) {
+        console.log(b, " is Largest");
+    } else {
+        console.log(c, " is Largest");
+    }
+}
